9dac120266b7343867334e1d93a663d7c3a26d6a
Scott Fasone <scott181182@yahoo.com>
2022-10-22 14:52:00 -0400

    feat(client): added tailwindcss and daisyui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.tsx",
+        "./components/**/*.tsx"
+    ],
+    theme: {
+        container: {
+            center: true
+        },
+        extend: {},
+    },
+    plugins: [
+        require("daisyui")
+    ],
+};
