d3802660ba842c6c02852b4a97b836a984e5aa8e
Scott Fasone <scott181182@yahoo.com>
2022-10-23 03:06:42 -0400

    feat(client): wip daily report page with step slider

--- frontend/pages/daily-report.tsx
+++ frontend/pages/daily-report.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 
 
 const DailyReportPage: NextPage = () => {
+    const [ step, setStep ] = useState(0);
+
     return (
-        <div className="p-8 flex-grow">
-            <h1>Daily Report Page</h1>
-        </div>
+        <>
+            <div className="p-8 flex-grow">
+                <h1>Daily Report Page</h1>
+            </div>
+            <ul className="steps w-full py-4">
+                <li className="step step-primary">Therapeutics</li>
+                <li className={step >= 1 ? "step step-primary" : "step"}>Symptoms</li>
+            </ul>
+        </>
     );
 };
 export default DailyReportPage;
