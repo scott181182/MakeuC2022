c7d5f0901f6ef5b0fa6d515bb1bb99aafbaff28f
Scott Fasone <scott181182@yahoo.com>
2022-10-23 02:38:38 -0400

    feat(client): added breadcrumbs and restructued nav

--- frontend/components/TopAppBar.tsx
+++ frontend/components/TopAppBar.tsx
@@ -1,38 +1,25 @@
 import Link from "next/link";
-import { useState } from "react";
-import { MdMailOutline, MdSettings, MdMenu } from "react-icons/md";
+import { useRouter } from "next/router";
+import { useMemo, useState } from "react";
+import { MdMailOutline, MdSettings } from "react-icons/md";
 
 
 
-// export interface TopAppBarProps {
-//     menuOpen: boolean;
-//     setMenuOpen: (value: boolean) => void;
-// }
-
-const NAV_ITEMS = [
-    {
-        label: "Homepage",
-        route: "/"
-    }, {
-        label: "Daily Report",
-        route: "/daily-report"
-    }, {
-        label: "Profile",
-        route: "/profile"
-    }, {
-        label: "Tutorials",
-        route: "/tutorials"
-    }, {
-        label: "My Privacy",
-        route: "/privacy"
-    }, {
-        label: "Contact Us",
-        route: "/contact"
-    }
-];
+function labelizeRoute(route: string) {
+    return route === "" ? "Homepage" : route
+        .replaceAll("-", " ")
+        .replace(/(^| )[a-z]/g, (val) => val.toUpperCase());
+}
 
 export function TopAppBar() {
+    const router = useRouter();
     const [ hasMail, setHasMail ] = useState(false);
+
+    const crumbs = useMemo(() => {
+        const routeParts = router.route === "/" ? [ "" ] : router.route.split("/");
+        return routeParts.map((p, i) => ({ label: labelizeRoute(p), route: "/" + routeParts.slice(0, i + 1).join("/") }));
+    }, [ router.route ]);
+    console.log(crumbs);
 
     // TODO: animate mail icon when mail is present
     const mailIcon = hasMail ?
@@ -42,35 +29,31 @@ export function TopAppBar() {
         </div> : <MdMailOutline/>;
 
     return (
-        <div className="navbar bg-primary h-24">
-            <div className="navbar-start">
-                {/* <button className="btn btn-lg btn-ghost btn-circle" onClick={() => setMenuOpen(!menuOpen)}>
-                    <MdMenu/>
-                </button> */}
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle">
-                        <MdMenu/>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {NAV_ITEMS.map((n) => <li key={n.label}><Link href={n.route}><a>{n.label}</a></Link></li>)}
+        <>
+            <div className="navbar bg-primary h-24">
+                <div className="navbar-start h-full">
+                    <Link href="/">
+                        <a className="h-full">
+                            <img src="/ivy-tag.png" alt="Ivy" className="h-full"/>
+                        </a>
+                    </Link>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn btn-lg btn-ghost btn-circle">
+                        {mailIcon}
+                    </button>
+                    <button className="btn btn-lg btn-ghost btn-circle mx-4">
+                        <MdSettings/>
+                    </button>
+                </div>
+            </div>
+            <div className="w-full bg-secondary px-4">
+                <div className="breadcrumbs">
+                    <ul>
+                        {crumbs.map((n) => <li key={n.label}><Link href={n.route}><a>{n.label}</a></Link></li>)}
                     </ul>
                 </div>
             </div>
-            <div className="navbar-center h-full">
-                <Link href="/">
-                    <a className="h-full">
-                        <img src="/ivy-tag.png" alt="Ivy" className="h-full"/>
-                    </a>
-                </Link>
-            </div>
-            <div className="navbar-end">
-                <button className="btn btn-lg btn-ghost btn-circle">
-                    {mailIcon}
-                </button>
-                <button className="btn btn-lg btn-ghost btn-circle">
-                    <MdSettings/>
-                </button>
-            </div>
-        </div>
+        </>
     );
 }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,16 +2,14 @@ import "../styles/globals.css";
 
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { IconContext } from "react-icons";
 
 import { client as apolloClient } from "../lib/apollo";
 import { TopAppBar } from "../components/TopAppBar";
-// import { SideNavBar } from "../components/SideNavBar";
 
 function App({ Component, pageProps }: AppProps) {
-    // const [ menuOpen, setMenuOpen ] = useState(false);
-
     const fontSettings: IconContext = {
         size: "3rem",
         style: {
@@ -22,13 +20,6 @@ function App({ Component, pageProps }: AppProps) {
         <ApolloProvider client={apolloClient}>
             <IconContext.Provider value={fontSettings}>
                 {(pageProps as any).navbar === false ? null : <TopAppBar/>}
-                {/* {(pageProps as any).navbar === false ? null : <TopAppBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>} */}
-                {/* <div className="flex">
-                    <SideNavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-                    <main className="flex-grow">
-                        <Component {...pageProps}/>
-                    </main>
-                </div> */}
                 <Component {...pageProps}/>
             </IconContext.Provider>
         </ApolloProvider>
