563bb9b47b986766a043825c45a7e13aa4291f34
Nick Lay <layns@mail.uc.edu>
2022-10-22 20:40:39 -0400

    add Postgres RDS config

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,21 +7,25 @@
 
 import { config } from "@keystone-6/core";
 
+
 // to keep this file tidy, we define our schema in a different file
 import { lists } from "./src/schema";
 
 // authentication is configured separately here too, but you might move this elsewhere
 // when you write your list-level access control functions, as they typically rely on session data
-import { withAuth, session } from "./src/auth";
+import { session, withAuth } from "./src/auth";
 
+import { POSTGRES_CON_STRING } from "./src/env";
 export default withAuth(
     config({
         db: {
             // we're using sqlite for the fastest startup experience
             //   for more information on what database might be appropriate for you
             //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-            provider: "sqlite",
-            url: "file:./keystone.db"
+            provider: "postgresql",
+            url: POSTGRES_CON_STRING,
+            enableLogging: true,
+            useMigrations: true
         },
         server: {
             port: 3001
